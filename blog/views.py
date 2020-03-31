54fbb9a754592359b059acf735546758dd58825f
BuildTools <unconfigured@null.spigotmc.org>
2020-03-31 16:58:18 -0700

    removed auth

--- blog/views.py
+++ blog/views.py
@@ -49,17 +49,11 @@ def writer(request):
         if form.is_valid():
             messages.success(request, 'form is valid')
             data = form.cleaned_data
-            user = User.objects.filter(username=data['username'])[0]
-            if user.groups.filter(name='admin') == 1:
-                messages.success(request, 'you are a admin')
-                if user.check_password(data['password']):
-                    messages.success(request, ' your password is correct')
-                    ar = Project.objects.create(
-                        title=data['project_title'], content=data['project_text'], date_posted=timezone.now(), category=data['project_category'], video=data['project_video'])
-                    ar.save()
-                    messages.success(request, 'your project has been posted')
-                else:
-                    messages.warning(request, 'Auth Failed')
+            ar = Project.objects.create(
+                title=data['project_title'], content=data['project_text'], date_posted=timezone.now(), category=data['project_category'], video=data['project_video'])
+            ar.save()
+            messages.success(request, 'your project has been posted')
+
         return render(request, 'blog/writer.html', {'form': new_project_form})
 
     else:
