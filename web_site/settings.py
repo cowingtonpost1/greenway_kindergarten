e860848facd9cd6bcdb49e166b9d280e74e44581
BuildTools <unconfigured@null.spigotmc.org>
2020-03-31 15:35:28 -0700

    added key

--- web_site/settings.py
+++ web_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# WARNING WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'qa$@7(bh+e$!ra5s!lk7ak8zsbx_y@w+&0q_hu5(_2-xecf34j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
